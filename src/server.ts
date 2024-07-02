764400035ae40b93061b1fb351a96c7efdd0b542
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-02 16:32:59 -0300

    feat: add e config fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-export {};
+import fastify, { FastifyInstance } from 'fastify';
+
+const app: FastifyInstance = fastify();
+
+app.listen(
+  {
+    port: 3000,
+  },
+  () => console.log('Server running on PORT 3000')
+);
